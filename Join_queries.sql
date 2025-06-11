eb993898dcd28431b747739c1e2e34856d3e1ce8
Harpreet Singh <harpreet.canadaucw@gmail.com>
2025-06-10 22:36:44 -0700

    Update Join_queries.sql

--- Join_queries.sql
+++ Join_queries.sql
@@ -2,3 +2,9 @@ SELECT prime_ministers.country, prime_ministers.continent, prime_minister, presi
 FROM presidents
 INNER JOIN prime_ministers
 ON presidents.country=prime_ministers.country;
+
+--INNER JOIN USING ALIASED TABLES
+SELECT p2.country, p2.continent,prime_minister, president
+FROM presidents AS p1
+INNER JOIN prime_ministers AS p2
+ON p1.country=p2.country;
